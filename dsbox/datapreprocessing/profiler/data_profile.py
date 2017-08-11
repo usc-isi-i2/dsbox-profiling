4f3ace64fccf999c471108399bd0f6b58f718b2d
Ke-Thia Yao <kyao@isi.edu>
2017-08-11 14:14:39 -0700

    Modify print and except statements to be python 3 compatible

--- dsbox/datapreprocessing/profiler/data_profile.py
+++ dsbox/datapreprocessing/profiler/data_profile.py
@@ -9,7 +9,7 @@ import feature_compute_lfh as fc_lfh
 import feature_compute_hih as fc_hih
 from collections import defaultdict
 
-def profile_data(data_path, punctuation_outlier_weight=3, 
+def profile_data(data_path, punctuation_outlier_weight=3,
         numerical_outlier_weight=3, token_delimiter=" ", detect_language=False, topk=10):
     """
     Main function to profile the data.
@@ -19,7 +19,7 @@ def profile_data(data_path, punctuation_outlier_weight=3,
     ----------
     """
     # STEP 1: get dependency and read data
-    
+
     isDF = False
     ## csv as input ##
     if not isinstance(data_path, pd.DataFrame):
@@ -37,18 +37,18 @@ def profile_data(data_path, punctuation_outlier_weight=3,
 
     corr_columns = list(corr_pearson.columns)
 
-    print "====================have a look on the data: ====================\n"
-    print data.head()
+    print("====================have a look on the data: ====================\n")
+    print(data.head())
 
     # STEP 2: calculations
-    
-    print "====================calculating the features ... ====================\n"
+
+    print("====================calculating the features ... ====================\n")
     result = {} # final result: dict of dict
     for column_name in data:
         col = data[column_name]
         # dict: map feature name to content
         each_res = defaultdict(lambda: defaultdict())
-        
+
         if column_name in corr_columns:
             corr_dict = {}
             corr_dict["columns"] = corr_columns
@@ -60,28 +60,28 @@ def profile_data(data_path, punctuation_outlier_weight=3,
         if col.dtype.kind in np.typecodes['AllInteger']+'uMmf':
             each_res["missing"]["num_missing"] = pd.isnull(col).sum()
             each_res["missing"]["num_nonblank"] = col.count()
-            each_res["special_type"]["dtype"] = str(col.dtype) 
+            each_res["special_type"]["dtype"] = str(col.dtype)
             ndistinct = col.nunique()
             each_res["distinct"]["num_distinct_values"] = ndistinct
             each_res["distinct"]["ratio_distinct_values"] = ndistinct/ float(col.size)
-        
+
         if col.dtype.kind == 'b':
             each_res["special_type"]["data_type"] = 'bool'
             fc_hih.compute_common_values(col.dropna().astype(str), each_res, topk)
-            
+
         elif col.dtype.kind in np.typecodes['AllInteger']+'u':
             each_res["special_type"]["data_type"] = 'integer'
             fc_hih.compute_numerics(col, each_res)
             fc_hih.compute_common_values(col.dropna().astype(str), each_res,topk)
-       
+
         elif col.dtype.kind == 'f':
             each_res["special_type"]["data_type"] = "float"
             fc_hih.compute_numerics(col, each_res)
             fc_hih.compute_common_values(col.dropna().astype(str), each_res,topk)
- 
+
         elif col.dtype.kind == 'M':
-            each_res["special_type"]["data_type"] = "datetime" 
-            
+            each_res["special_type"]["data_type"] = "datetime"
+
         elif col.dtype.kind == 'm':
             each_res["special_type"]["data_type"] = "timedelta"
 
@@ -93,7 +93,7 @@ def profile_data(data_path, punctuation_outlier_weight=3,
 
             # compute_missing_space Must be put as the first one because it may change the data content, see function def for details
             fc_lfh.compute_missing_space(col, each_res)
-            fc_lfh.compute_filename(col, each_res)    
+            fc_lfh.compute_filename(col, each_res)
             fc_lfh.compute_length_distinct(col, each_res, delimiter=token_delimiter)
             if detect_language: fc_lfh.compute_lang(col, each_res)
             fc_lfh.compute_punctuation(col, each_res, weight_outlier=punctuation_outlier_weight)
@@ -106,15 +106,15 @@ def profile_data(data_path, punctuation_outlier_weight=3,
             fc_hih.compute_numeric_density(col, each_res)
             fc_hih.compute_contain_numeric_values(col, each_res)
             fc_hih.compute_common_tokens_by_puncs(col, each_res, topk)
-       
+
         if not each_res["numeric_stats"]: del each_res["numeric_stats"]
-        
+
         result[column_name] = each_res # add this column features into final result
 
-    print "====================calculations finished ====================\n"
+    print("====================calculations finished ====================\n")
 
     return result
-    
+
 
 if __name__ == '__main__':
     """
@@ -123,10 +123,9 @@ if __name__ == '__main__':
     result = profile_data(sys.argv[1])
     output_filename = sys.argv[2]
     # wirting JSON formated output
-    print "     ====================>> wirting to file: {}\n".format(output_filename)
+    print("     ====================>> wirting to file: {}\n".format(output_filename))
     output_json = json.dumps(result, indent=4)
     f = open(output_filename, 'w')
     f.write(output_json)
     f.close()
-    print  "======================ALL DONE ===================="
-
+    print ("======================ALL DONE ====================")
