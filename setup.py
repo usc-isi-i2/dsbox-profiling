b894740eae04427f6e12a425c7dd3a5f1f3b7450
Ke-Thia Yao <kyao@isi.edu>
2017-09-28 21:53:43 -0700

    Update setup to use d3m entry points

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup
 
-setup(name='dsbox-datapreprocessing',
-      version='0.2.0',
+setup(name='dsbox-dataprofiling',
+      version='0.2.1',
       url='https://github.com/usc-isi-i2/dsbox-cleaning.git',
       maintainer_email='kyao@isi.edu',
       maintainer='Ke-Thia Yao',
-      description='DSBox data preprocssing tools',
+      description='DSBox data profiling  tools',
       license='MIT',
       packages=['dsbox', 'dsbox.datapreprocessing', 'dsbox.datapreprocessing.profiler'],
       zip_safe=False,
       python_requires='>=3.5',
       install_requires=[
           'numpy>=1.11.1', 'pandas>=0.20.1', 'langdetect>=1.0.7'
-      ]
+      ],
+      entry_points = {
+          'd3m.primitives': [
+              'dsbox.Profiler = dsbox.datapreprocessingf.profiler:Profiler',
+          ],
+      },
+
 )
