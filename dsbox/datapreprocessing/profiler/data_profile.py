f1134a139b3b72fd3951524e3a45b217316bc3c1
fanghao <fanghao.luo@gmail.com>
2018-01-03 15:18:28 -0800

    fit with metadata

--- dsbox/datapreprocessing/profiler/data_profile.py
+++ dsbox/datapreprocessing/profiler/data_profile.py
@@ -3,6 +3,7 @@ import json
 import sys
 import time
 import numpy as np  # type: ignore
+import typing
 
 
 # from feature computation functions
@@ -10,147 +11,131 @@ from . import feature_compute_lfh as fc_lfh
 from . import feature_compute_hih as fc_hih
 from collections import defaultdict
 
-from typing import Dict
 from primitive_interfaces.transformer import TransformerPrimitiveBase
-from d3m_metadata.container.pandas import DataFrame
+from d3m_metadata import container, hyperparams, metadata
 
 # TODO: change this to `from d3m_metadata import metadata` once confirm the code
 import sys 
-sys.path.append("/Users/luofanghao/work/USC_lab/isi-II/work/DSBox_project/end2end_sys/metadata/d3m_metadata") 
-import metadata
+sys.path.insert(0, "/Users/luofanghao/work/USC_lab/isi-II/work/DSBox_project/end2end_sys/metadata/") 
+from d3m_metadata import container
+from d3m_metadata.container import dataset
 
-Input = DataFrame
-Output = metadata.Metadata
+# Input = typing.Union[dataset.Dataset, \
+#                     DataFrame]
+Input = container.DataFrame
+Output = container.Dataset
 
-class Profiler(TransformerPrimitiveBase[Input, Output, None]):
-    
-    __author__ = "USC ISI"
-    __metadata__ = {
-            "id": "b2612849-39e4-33ce-bfda-24f3e2cb1e93",
-            "name": "dsbox.datapreprocessing.profiler.Profiler",
-            "common_name": "DSBox Data Profiler",
-            "description": "Generate profile of tabular data in json, including language_detection, missing_data_detection, distinct_values, value_length, numerical_stats, frequency, correlation and others",
-            "languages": [ "python3.5", "python3.6" ],
-            "library": "dsbox",
-            "version": "0.2.0",
-            "is_class": True,
-            "parameters": [
-                {
-                    "name": "punctuation_outlier_weight",
-                    "description": "Outlier distance in number of standard deviations",
-                    "type": "float",
-                    "default":"3",
-                    "is_hyperparameter": True
-                    },
-                {
-                    "name": "numerical_outlier_weight",
-                    "description": "Outlier distance in number of standard deviations",
-                    "type": "float",
-                    "default": "3",
-                    "is_hyperparameter": True
-                    },
-                {
-                    "name": "token_delimiter",
-                    "description": "Character to separate word tokens. Default is space.",
-                    "type": "character",
-                    "default": " ",
-                    "is_hyperparameter": True
-                    },
-                {
-                    "name": "detect_language",
-                    "description": "Detect language (e.g. English) from text fields",
-                    "type": "character",
-                    "default": "false",
-                    "is_hyperparameter": False
-                    },
-                {
-                    "name": "topk",
-                    "description": "Number of top values to return",
-                    "type": "integer",
-                    "default": "10",
-                    "is_hyperparameter": False
-                    },
-                {
-                    "name": "verbose",
-                    "description": "Generate extra dianostic output",
-                    "type": "boolean",
-                    "default": "false",
-                    "is_hyperparameter": False
-                    }
-                ],
-            "task_type": [ "Data preprocessing" ],
-            "tags": [ "preprocessing", "profile" ],
-            "build": [{ "type": "pip", "package": "dsbox-dataprofiling" }],
-            "team": "USC ISI",
-            "schema_version": 1.0,
-            "interfaces": [ "TransformerPrimitiveBase" ],
-            "interfaces_version": "2017.9.22rc0",
-            "compute_resources": {
-                    "cores_per_node": [],
-                    "disk_per_node": [],
-                    "expected_running_time": [],
-                    "gpus_per_node": [],
-                    "mem_per_gpu": [],
-                    "mem_per_node": [],
-                    "num_nodes": [],
-                    "sample_size": [],
-                    "sample_unit": []
-                    }
-            }
-    
+class Hyperparams(hyperparams.Hyperparams):
+    """
+    No hyper-parameters for this primitive.
+    """
+
+    pass
+
+
+
+class Profiler(TransformerPrimitiveBase[Input, Output, Hyperparams]):
     """
     data profiler moduel. Now only supports csv data.
 
     Parameters:
     ----------
-    punctuation_outlier_weight: a integer
+    _punctuation_outlier_weight: a integer
         the coefficient used in outlier detection for punctuation. default is 3
 
-    numerical_outlier_weight
+    _numerical_outlier_weight
 
-    token_delimiter: a string
+    _token_delimiter: a string
         delimiter that used to seperate tokens, default is blank space " ".
 
-    detect_language: boolean
+    _detect_language: boolean
         true: do detect language; false: not detect language
 
-    topk: a integer
+    _topk: a integer
 
 
-    verbose: boolean
-        control the verbose
+    _verbose: boolean
+        control the _verbose
 
     Attributes:
     ----------
 
     """
+    __author__ = 'USC ISI'
+    __version__ = '0.2.0'
+    metadata = metadata.PrimitiveMetadata({
+            'id': 'b2612849-39e4-33ce-bfda-24f3e2cb1e93',
+            'version': __version__,
+            'name': "Profiler",
+            'keywords': ['profiler'],
+            'source': {
+                'name': __author__,
+                'uris': [
+                    # Unstructured URIs. Link to file and link to repo in this case.
+                    'https://github.com/usc-isi-i2/dsbox-profiling/blob/master/dsbox/datapreprocessing/profiler/data_profile.py',
+                    'https://github.com/usc-isi-i2/dsbox-profiling.git',
+                    ],
+            },
+            # The same path the primitive is registered with entry points in setup.py.
+            'installation': [{
+                'type': metadata.PrimitiveInstallationType.PIP,
+                'package_uri': 'https://github.com/usc-isi-i2/dsbox-profiling.git'
+            }],
+            'python_path': 'd3m.primitives.dsbox.profiler',
+            # Choose these from a controlled vocabulary in the schema. If anything is missing which would
+            # best describe the primitive, make a merge request.
 
-    def __init__(self, punctuation_outlier_weight=3,
-        numerical_outlier_weight=3, token_delimiter=" ",
-        detect_language=False, topk=10, verbose=False):
-        self.punctuation_outlier_weight = punctuation_outlier_weight
-        self.numerical_outlier_weight = numerical_outlier_weight
-        self.token_delimiter = token_delimiter
-        self.detect_language = detect_language
-        self.topk = topk
-        self.verbose = verbose
-        self.results = metadata.Metadata()  # profiler result
-        self.counter = 0
+            'algorithm_types': [
+                metadata.PrimitiveAlgorithmType.DATA_PROFILING,
+            ],
+            'primitive_family': metadata.PrimitiveFamily.DATA_PREPROCESSING,
+            # A metafeature about preconditions required for this primitive to operate well.
+
+            # for profiler, maybe no precon
+            "precondition": []
+        })
+    
+    
+    def __init__(self, *, hyperparams: Hyperparams, random_seed: int = 0, 
+            docker_containers: typing.Union[typing.Dict[str, str], None] = None) -> None:
+        # All primitives must define these attributes
+        self.hyperparams = hyperparams
+        self.random_seed = random_seed
+        self.docker_containers = docker_containers
+
+        # All other attributes must be private with leading underscore  
+        self._punctuation_outlier_weight = 3
+        self._numerical_outlier_weight = 3
+        self._token_delimiter = " "
+        self._detect_language = False
+        self._topk = 10
+        self._verbose = hyperparams['verbose'] if hyperparams else 0
 
 
 
     def produce(self, *, inputs: Input, timeout: float = None, iterations: int = None) -> Output:
-        if isinstance(inputs, pd.DataFrame):
-            return self.profile_data(inputs)
+        if isinstance(inputs, dataset.Dataset):
+            # perhaps multiple tables
+            # return self._profile_data(inputs)
+            self.results = inputs.metadata
+            for table_id in inputs:
+                dataframe = pd.DataFrame(data = inputs[table_id])
+                self._profile_data(dataframe, table_id)    # inplace manipulate
+
+            inputs.metadata = self.results
+            return inputs
         else:
-            for x in inputs:
-                self.profile_data(x)    # inplace manipulate
-                self.counter += 1
+            # single table
+            key = "0" # default key for single table
+            self.results = metadata.Metadata()
+            self._profile_data(inputs[0], key)
+ 
+            
+            ds = dataset.Dataset({key: input[0]}, self.results)
+            return ds
+            
 
-            self.counter = 0
-            return self.results
-
-    def profile_data(self, data_path):
+    def _profile_data(self, data_path, table_id="0"):
 
         """
         Main function to profile the data.
@@ -178,15 +163,17 @@ class Profiler(TransformerPrimitiveBase[Input, Output, None]):
 
         corr_columns = list(corr_pearson.columns)
 
-        if self.verbose:
+        if self._verbose:
             print("====================have a look on the data: ====================\n")
             print(data.head())
 
         # STEP 2: calculations
-        if self.verbose:
+        if self._verbose:
             print("====================calculating the features ... ====================\n")
         result = {} # final result: dict of dict
+        column_counter = -1
         for column_name in data:
+            column_counter += 1
             col = data[column_name]
             # dict: map feature name to content
             each_res = defaultdict(lambda: defaultdict())
@@ -209,17 +196,17 @@ class Profiler(TransformerPrimitiveBase[Input, Output, None]):
 
             if col.dtype.kind == 'b':
                 each_res["special_type"]["data_type"] = 'bool'
-                fc_hih.compute_common_values(col.dropna().astype(str), each_res, self.topk)
+                fc_hih.compute_common_values(col.dropna().astype(str), each_res, self._topk)
 
             elif col.dtype.kind in np.typecodes['AllInteger']+'u':
                 each_res["special_type"]["data_type"] = 'integer'
                 fc_hih.compute_numerics(col, each_res)
-                fc_hih.compute_common_values(col.dropna().astype(str), each_res,self.topk)
+                fc_hih.compute_common_values(col.dropna().astype(str), each_res,self._topk)
 
             elif col.dtype.kind == 'f':
                 each_res["special_type"]["data_type"] = "float"
                 fc_hih.compute_numerics(col, each_res)
-                fc_hih.compute_common_values(col.dropna().astype(str), each_res,self.topk)
+                fc_hih.compute_common_values(col.dropna().astype(str), each_res,self._topk)
 
             elif col.dtype.kind == 'M':
                 each_res["special_type"]["data_type"] = "datetime"
@@ -236,26 +223,26 @@ class Profiler(TransformerPrimitiveBase[Input, Output, None]):
                 # compute_missing_space Must be put as the first one because it may change the data content, see function def for details
                 fc_lfh.compute_missing_space(col, each_res)
                 fc_lfh.compute_filename(col, each_res)
-                fc_lfh.compute_length_distinct(col, each_res, delimiter=self.token_delimiter)
-                if self.detect_language: fc_lfh.compute_lang(col, each_res)
-                fc_lfh.compute_punctuation(col, each_res, weight_outlier=self.punctuation_outlier_weight)
+                fc_lfh.compute_length_distinct(col, each_res, delimiter=self._token_delimiter)
+                if self._detect_language: fc_lfh.compute_lang(col, each_res)
+                fc_lfh.compute_punctuation(col, each_res, weight_outlier=self._punctuation_outlier_weight)
 
                 fc_hih.compute_numerics(col, each_res)
-                fc_hih.compute_common_numeric_tokens(col, each_res,self.topk)
-                fc_hih.compute_common_alphanumeric_tokens(col, each_res, self.topk)
-                fc_hih.compute_common_values(col, each_res, self.topk)
-                fc_hih.compute_common_tokens(col, each_res, self.topk)
+                fc_hih.compute_common_numeric_tokens(col, each_res,self._topk)
+                fc_hih.compute_common_alphanumeric_tokens(col, each_res, self._topk)
+                fc_hih.compute_common_values(col, each_res, self._topk)
+                fc_hih.compute_common_tokens(col, each_res, self._topk)
                 fc_hih.compute_numeric_density(col, each_res)
                 fc_hih.compute_contain_numeric_values(col, each_res)
-                fc_hih.compute_common_tokens_by_puncs(col, each_res, self.topk)
+                fc_hih.compute_common_tokens_by_puncs(col, each_res, self._topk)
 
             if not each_res["numeric_stats"]: del each_res["numeric_stats"]
 
             # result[column_name] = each_res # add this column features into final result
 
-            self.results = self.results.update((self.counter, column_name,), each_res)
+            self.results = self.results.update((table_id, metadata.ALL_ELEMENTS, column_counter,), each_res)
 
-        if self.verbose: print("====================calculations finished ====================\n")
+        if self._verbose: print("====================calculations finished ====================\n")
 
 
         return self.results
@@ -276,7 +263,7 @@ if __name__ == '__main__':
     main function to execute profiler
     """
     profiler = Profiler()
-    result = profiler.profile_data(sys.argv[1])
+    result = profiler._profile_data(sys.argv[1])
     output_filename = sys.argv[2]
     # wirting JSON formated output
     print("     ====================>> wirting to file: {}\n".format(output_filename))
--- example.py
+++ example.py
@@ -1,4 +1,16 @@
 from dsbox.datapreprocessing.profiler import Profiler
+from d3m_metadata.container import dataset 
+from d3m_metadata import hyperparams, metadata
 
-profiler = Profiler()
-jsonResult = profiler.profile_data('testData.csv')
+class Hyperparams(hyperparams.Hyperparams):
+    pass
+
+# 1. dataset as input
+dataset_doc_path = "/Users/luofanghao/work/USC_lab/isi-II/work/DSBox_project/end2end_sys/metadata/tests/data/datasets/iris_dataset_1/datasetDoc.json"
+ds = dataset.Dataset.load('file://{dataset_doc_path}'.format(dataset_doc_path=dataset_doc_path))
+
+profiler = Profiler(hyperparams=Hyperparams())
+result = profiler.produce(inputs=ds)
+
+result.metadata.query(("0", "1", 1,) )
+result.metadata.query(("0", metadata.ALL_ELEMENTS, 1,) )
